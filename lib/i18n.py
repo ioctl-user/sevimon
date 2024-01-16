f9b8bbe97a3391933e27592be12051573f33ad5b
ioctl-user <nomail>
2024-01-16 14:14:53 +0300

    Add a hack to detect locale on the Mac OS.

--- lib/i18n.py
+++ lib/i18n.py
@@ -1,9 +1,18 @@
 import locale
+import sys
+import os
 from lib.locale.init import translation
 from lib.locale.ru import *
 
-# The locale.getdefaultlocale() is obsolete, so this code should be updated
-curr_lang = locale.getdefaultlocale()[0].split('_')[0]
+if sys.platform != 'darwin':
+    # The locale.getdefaultlocale() is obsolete, so this code should be updated
+    curr_lang = locale.getdefaultlocale()[0].split('_')[0]
+else:
+    # Standard locale app doesn't work correct in the Mac OS, so use a hack
+    sys_lang = os.popen("defaults read -g AppleLanguages").read()
+    str_lang = sys_lang.translate({ord(i): None for i in '()"\n '})
+    curr_lang = str_lang.split('-')[0]
+
 
 def _(arg):
     if curr_lang in translation:
